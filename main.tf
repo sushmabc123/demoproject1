a07d77e88e4d8038f5a41288cca03c9d842f2fd9
sushmabc123 <160147913+sushmabc123@users.noreply.github.com>
2024-02-21 08:45:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
-provider "aws" 
-{
+provider "aws" {
   region = "us-east-1" 
 }
 
-resource "aws_instance" "myinstance5"
-{
+resource "aws_instance" "myinstance5"{
   ami           = "ami-0e731c8a588258d0d"  
   instance_type = "t2.micro"                
 }
