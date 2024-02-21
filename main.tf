8021828fe82bb3aaa4d677197a7ad0842cf405b3
sushmabc123 <160147913+sushmabc123@users.noreply.github.com>
2024-02-21 08:29:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-provider "aws" {
+provider "aws" 
+{
   region = "us-east-1" 
 }
 
-resource "aws_instance" "myinstance5" {
-  ami           = "ami-0e731c8a588258d0d"  # Change this to your desired AMI
-  instance_type = "t2.micro"                # Change this to your desired instance type
+resource "aws_instance" "myinstance5"
+{
+  ami           = "ami-0e731c8a588258d0d"  
+  instance_type = "t2.micro"                
 }
